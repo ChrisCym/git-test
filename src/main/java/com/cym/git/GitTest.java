ecfa91e549d5561aee05089e14ce8e098409f223
Chris <297994784@qq.com>
2021-06-22 16:53:22 +0800

    first commit

---
+++ src/main/java/com/cym/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cym.git;
+
+/**
+ * @author : cym
+ * @date 2021/6/22.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
